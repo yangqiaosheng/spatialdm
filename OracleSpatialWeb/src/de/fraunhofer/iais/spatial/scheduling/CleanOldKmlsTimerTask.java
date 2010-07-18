699301fae983ada713c6c42a844d19f9e102970b
haolin.zhi <haolin.zhi@62b768d2-f648-7287-3a29-ed549c025e75>
2010-07-18 22:24:54 +0000

    slf4j instead of commons-logging

--- OracleSpatialWeb/src/de/fraunhofer/iais/spatial/scheduling/CleanOldKmlsTimerTask.java
+++ OracleSpatialWeb/src/de/fraunhofer/iais/spatial/scheduling/CleanOldKmlsTimerTask.java
@@ -5,20 +5,21 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimerTask;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.fraunhofer.iais.spatial.servlet.RequestKml;
 
 /**
  * Timer Task to delete the old temporary kml files in the kml\ directory
- * @author zhi
+ * @author haolin_zhi
  *
  */
 public class CleanOldKmlsTimerTask extends TimerTask {
 	/**
 	 * Logger for this class
 	 */
-	private static final Logger logger = Logger.getLogger(CleanOldKmlsTimerTask.class);
+	private static final Logger logger = LoggerFactory.getLogger(CleanOldKmlsTimerTask.class);
 
 	// delay deleting period to avoid threads competition,
 	// format: "yyMMddHHmmss"
@@ -26,34 +27,26 @@ public class CleanOldKmlsTimerTask extends TimerTask {
 
 	@Override
 	public void run() {
-		if (logger.isDebugEnabled()) {
-			logger.debug("run() - start"); //$NON-NLS-1$
-		}
+		logger.debug("run() - start"); //$NON-NLS-1$
 
 		//		File kmlPath = new File("../webapps/OracleSpatialWeb/" + RequestKml.kmlPath);
 		//		File kmlPath = new File(this.getClass().getResource("/../../" + RequestKml.kmlPath).getPath());
 		File kmlPath = new File(System.getProperty("oraclespatialweb.root") + RequestKml.kmlPath);
 		File files[] = kmlPath.listFiles();
-		if (logger.isDebugEnabled()) {
-			logger.debug("run() - kmlPath:" + kmlPath.getAbsolutePath()); //$NON-NLS-1$
-		}
+		logger.debug("run() - kmlPath:" + kmlPath.getAbsolutePath()); //$NON-NLS-1$
 		long currentDate = Long.parseLong(new SimpleDateFormat("yyMMddHHmmss").format(new Date()));
 		for (File f : files) {
 			String filename = f.getName();
 			if (filename.length() == 25 && filename.matches("\\d{12}-\\w{8}.\\w{1}ml")) {
 				long fileDate = Long.parseLong(filename.substring(0, 12));
 				if (currentDate > fileDate + period) {
-					if (logger.isDebugEnabled()) {
-						logger.debug("run() - delete:" + filename); //$NON-NLS-1$
-					}
+					logger.debug("run() - delete:" + filename); //$NON-NLS-1$
 					f.delete();
 				}
 			}
 		}
 
-		if (logger.isDebugEnabled()) {
-			logger.debug("run() - end"); //$NON-NLS-1$
-		}
+		logger.debug("run() - end"); //$NON-NLS-1$
 	}
 
 }
