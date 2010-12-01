28e5c5f8d5353a3d572852f30e45f6d1b15907ba
haolin.zhi <haolin.zhi@62b768d2-f648-7287-3a29-ed549c025e75>
2010-12-01 16:08:42 +0000

    fix bug in Mybatis_spring integration clean up code

--- trunk/OracleSpatialWeb/test/de/fraunhofer/iais/spatial/test/TestUKRode.java
+++ trunk/OracleSpatialWeb/test/de/fraunhofer/iais/spatial/test/TestUKRode.java
@@ -11,23 +11,18 @@ import org.jdom.CDATA;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.Namespace;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
 import de.fraunhofer.iais.spatial.dao.jdbc.DB;
-import de.fraunhofer.iais.spatial.service.FlickrDeWestAreaMgr;
 import de.fraunhofer.iais.spatial.util.XmlUtil;
 
 @ContextConfiguration("classpath:beans.xml")
-public class TestUKRode extends AbstractJUnit4SpringContextTests{
+public class TestUKRode extends AbstractJUnit4SpringContextTests {
 
 	@Resource(name = "db")
 	private DB db;
-
 
 	@Test
 	public void testView() throws SQLException {
@@ -75,30 +70,28 @@ public class TestUKRode extends AbstractJUnit4SpringContextTests{
 			int linkCode = rset.getInt("LINK_CODE");
 			double avgSpeed = rset.getDouble("SPEED");
 
-			PreparedStatement selectStmt2 = db.getPstmt(conn,
-					"select t.LINK_CODE, t.LATITUDE_FROM, t.LONGITUDE_FROM, t.LATITUDE_TO, t.LONGITUDE_TO, t.LOCATION_CODE_FROM, t.LOCATION_CODE_TO from datasample_geom_view t"
-							+ " where t.LINK_CODE = ?");
+			PreparedStatement selectStmt2 = db.getPstmt(conn, "select t.LINK_CODE, t.LATITUDE_FROM, t.LONGITUDE_FROM, t.LATITUDE_TO, t.LONGITUDE_TO, t.LOCATION_CODE_FROM, t.LOCATION_CODE_TO from datasample_geom_view t" + " where t.LINK_CODE = ?");
 			selectStmt2.setInt(1, linkCode);
 			ResultSet rset2 = db.getRs(selectStmt2);
 			if (rset2.next()) {
 
 				String id = String.valueOf(linkCode);
 				String name = "link-id: " + String.valueOf(linkCode);
-				String description = "avg-speed: " + String.valueOf((float)avgSpeed) + "kph";
+				String description = "avg-speed: " + String.valueOf((float) avgSpeed) + "kph";
 				String latFrom = rset2.getString("LATITUDE_FROM");
 				String lngFrom = rset2.getString("LONGITUDE_FROM");
 				String latTo = rset2.getString("LATITUDE_TO");
 				String lngTo = rset2.getString("LONGITUDE_TO");
 
-				if(latFrom == null || lngFrom == null || latTo == null || lngTo == null){
+				if (latFrom == null || lngFrom == null || latTo == null || lngTo == null) {
 					continue;
 				}
 
-				String lineStyleWidth = String.valueOf((int)((avgSpeed-40)/15)+1);
+				String lineStyleWidth = String.valueOf((int) ((avgSpeed - 40) / 15) + 1);
 				String lineStyleColor = "FF0000";
 				String coordinates = lngFrom + "," + latFrom + ", 0 \n" + lngTo + "," + latTo + ", 0";
 
-				String redColor = Integer.toHexString((int) ((avgSpeed-40) / 130 * 255) & 0x000000FF);
+				String redColor = Integer.toHexString((int) ((avgSpeed - 40) / 130 * 255) & 0x000000FF);
 
 				// create kml
 				Element placemarkElement = new Element("Placemark", namespace);
@@ -153,93 +146,88 @@ public class TestUKRode extends AbstractJUnit4SpringContextTests{
 	}
 
 	@Test
-		public void testSimpleXmlfromView() throws SQLException {
+	public void testSimpleXmlfromView() throws SQLException {
 
-			long start = System.currentTimeMillis();
+		long start = System.currentTimeMillis();
 
-			Document document = new Document();
-			Element rootElement = new Element("Paths");
-			document.setRootElement(rootElement);
+		Document document = new Document();
+		Element rootElement = new Element("Paths");
+		document.setRootElement(rootElement);
 
+		Connection conn = db.getConn();
 
-			Connection conn = db.getConn();
+		PreparedStatement selectStmt = db.getPstmt(conn, "select t.LINK_CODE, AVG(t.MEASURED_SPEED_KPH) AS SPEED from datasample t group by (t.LINK_CODE)");
+		ResultSet rset = db.getRs(selectStmt);
+		int i = 0;
+		while (rset.next()) {
+			int linkCode = rset.getInt("LINK_CODE");
+			double avgSpeed = rset.getDouble("SPEED");
 
-			PreparedStatement selectStmt = db.getPstmt(conn, "select t.LINK_CODE, AVG(t.MEASURED_SPEED_KPH) AS SPEED from datasample t group by (t.LINK_CODE)");
-			ResultSet rset = db.getRs(selectStmt);
-			int i = 0;
-			while (rset.next()) {
-				int linkCode = rset.getInt("LINK_CODE");
-				double avgSpeed = rset.getDouble("SPEED");
+			PreparedStatement selectStmt2 = db.getPstmt(conn, "select t.LINK_CODE, t.LATITUDE_FROM, t.LONGITUDE_FROM, t.LATITUDE_TO, t.LONGITUDE_TO, t.LOCATION_CODE_FROM, t.LOCATION_CODE_TO from datasample_geom_view t" + " where t.LINK_CODE = ?");
+			selectStmt2.setInt(1, linkCode);
+			ResultSet rset2 = db.getRs(selectStmt2);
+			if (rset2.next()) {
 
-				PreparedStatement selectStmt2 = db.getPstmt(conn,
-						"select t.LINK_CODE, t.LATITUDE_FROM, t.LONGITUDE_FROM, t.LATITUDE_TO, t.LONGITUDE_TO, t.LOCATION_CODE_FROM, t.LOCATION_CODE_TO from datasample_geom_view t"
-								+ " where t.LINK_CODE = ?");
-				selectStmt2.setInt(1, linkCode);
-				ResultSet rset2 = db.getRs(selectStmt2);
-				if (rset2.next()) {
+				String id = String.valueOf(linkCode);
+				String description = String.valueOf((float) avgSpeed) + "kph";
+				String latFrom = rset2.getString("LATITUDE_FROM");
+				String lngFrom = rset2.getString("LONGITUDE_FROM");
+				String latTo = rset2.getString("LATITUDE_TO");
+				String lngTo = rset2.getString("LONGITUDE_TO");
 
-					String id = String.valueOf(linkCode);
-					String description = String.valueOf((float)avgSpeed) + "kph";
-					String latFrom = rset2.getString("LATITUDE_FROM");
-					String lngFrom = rset2.getString("LONGITUDE_FROM");
-					String latTo = rset2.getString("LATITUDE_TO");
-					String lngTo = rset2.getString("LONGITUDE_TO");
-
-					if(latFrom == null || lngFrom == null || latTo == null || lngTo == null){
-						continue;
-					}
-
-					String lineStyleWidth = String.valueOf((int)((avgSpeed-40)/15)+1);
-					String lineStyleColor = "FF0000";
-					String coordinates = lngFrom + "," + latFrom + ", 0 \n" + lngTo + "," + latTo + ", 0";
-
-					String redColor = Integer.toHexString((int) ((avgSpeed-40) / 130 * 255) & 0x000000FF);
-
-					// create kml
-					Element pathElement = new Element("Path");
-					rootElement.addContent(pathElement);
-
-					Element speedElement = new Element("Speed");
-					speedElement.addContent(description);
-
-					pathElement.setAttribute("id", id);
-					pathElement.addContent(speedElement);
-
-
-					Element lindWidthElement = new Element("LineWidth");
-					lindWidthElement.addContent(lineStyleWidth);
-					Element lineColorElement2 = new Element("LineColor");
-					lineColorElement2.addContent(lineStyleColor + redColor);
-					pathElement.addContent(lindWidthElement);
-					pathElement.addContent(lineColorElement2);
-
-					Element coordinatesElement = new Element("Coordinates");
-					pathElement.addContent(coordinatesElement);
-					coordinatesElement.addContent(coordinates);
+				if (latFrom == null || lngFrom == null || latTo == null || lngTo == null) {
+					continue;
 				}
 
-				db.close(rset2);
-				db.close(selectStmt2);
+				String lineStyleWidth = String.valueOf((int) ((avgSpeed - 40) / 15) + 1);
+				String lineStyleColor = "FF0000";
+				String coordinates = lngFrom + "," + latFrom + ", 0 \n" + lngTo + "," + latTo + ", 0";
+
+				String redColor = Integer.toHexString((int) ((avgSpeed - 40) / 130 * 255) & 0x000000FF);
+
+				// create kml
+				Element pathElement = new Element("Path");
+				rootElement.addContent(pathElement);
+
+				Element speedElement = new Element("Speed");
+				speedElement.addContent(description);
+
+				pathElement.setAttribute("id", id);
+				pathElement.addContent(speedElement);
+
+				Element lindWidthElement = new Element("LineWidth");
+				lindWidthElement.addContent(lineStyleWidth);
+				Element lineColorElement2 = new Element("LineColor");
+				lineColorElement2.addContent(lineStyleColor + redColor);
+				pathElement.addContent(lindWidthElement);
+				pathElement.addContent(lineColorElement2);
+
+				Element coordinatesElement = new Element("Coordinates");
+				pathElement.addContent(coordinatesElement);
+				coordinatesElement.addContent(coordinates);
+			}
+
+			db.close(rset2);
+			db.close(selectStmt2);
 
 //				if (i >= 5) {
 //					break;
 //				}
-				i++;
-			}
-
-			System.out.println("i:" + i);
-			db.close(rset);
-			db.close(selectStmt);
-			db.close(conn);
-			rset.close();
-			selectStmt.close();
-			conn.close();
-
-			XmlUtil.xml2File(document, "temp/testUK.xml", false);
-			XmlUtil.xml2String(document, false);
-
-			System.out.println("cost time: " + (System.currentTimeMillis() - start));
+			i++;
 		}
 
+		System.out.println("i:" + i);
+		db.close(rset);
+		db.close(selectStmt);
+		db.close(conn);
+		rset.close();
+		selectStmt.close();
+		conn.close();
+
+		XmlUtil.xml2File(document, "temp/testUK.xml", false);
+		XmlUtil.xml2String(document, false);
+
+		System.out.println("cost time: " + (System.currentTimeMillis() - start));
+	}
 
 }
