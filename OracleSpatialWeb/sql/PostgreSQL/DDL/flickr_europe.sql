7e68047b44abc0fef803f807921d995526b401b4
haolin.zhi <haolin.zhi@62b768d2-f648-7287-3a29-ed549c025e75>
2011-03-03 15:40:54 +0000

    update table schema for pgsql

--- OracleSpatialWeb/sql/PostgreSQL/DDL/flickr_europe.sql
+++ OracleSpatialWeb/sql/PostgreSQL/DDL/flickr_europe.sql
@@ -1,4 +1,4 @@
-﻿-- Table: flickr_europe
+-- Table: flickr_europe
 
 -- DROP TABLE flickr_europe;
 
@@ -10,23 +10,44 @@ CREATE TABLE flickr_europe
   latitude numeric(15,0) NOT NULL,
   taken_date date,
   upload_date date,
-  viewed numeric,
+  viewed integer,
   title character varying(255),
   smallurl character varying(100),
   place_id character varying(20),
   woe_id character varying(20),
-  accuracy numeric,
+  accuracy integer,
   region_checked integer NOT NULL DEFAULT 0,
-  region_5000_id numeric,
-  region_10000_id numeric,
-  region_20000_id numeric,
-  region_40000_id numeric,
-  region_80000_id numeric,
-  region_160000_id numeric,
-  region_320000_id numeric,
+  region_5000_id integer,
+  region_10000_id integer,
+  region_20000_id integer,
+  region_40000_id integer,
+  region_80000_id integer,
+  region_160000_id integer,
+  region_320000_id integer,
   CONSTRAINT flickr_euorpe_pk PRIMARY KEY (photo_id),
   CONSTRAINT flickr_europe_fk FOREIGN KEY (user_id)
       REFERENCES flickr_people (user_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT flickr_europe_r10000_fk FOREIGN KEY (region_10000_id)
+      REFERENCES flickr_europe_area_10000 (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT flickr_europe_r160000_fk FOREIGN KEY (region_160000_id)
+      REFERENCES flickr_europe_area_160000 (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT flickr_europe_r20000_fk FOREIGN KEY (region_20000_id)
+      REFERENCES flickr_europe_area_20000 (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT flickr_europe_r320000_fk FOREIGN KEY (region_320000_id)
+      REFERENCES flickr_europe_area_320000 (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT flickr_europe_r40000_fk FOREIGN KEY (region_40000_id)
+      REFERENCES flickr_europe_area_40000 (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT flickr_europe_r5000_fk FOREIGN KEY (region_5000_id)
+      REFERENCES flickr_europe_area_5000 (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT flickr_europe_r80000_fk FOREIGN KEY (region_80000_id)
+      REFERENCES flickr_europe_area_80000 (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
--- OracleSpatialWeb/sql/PostgreSQL/DDL/flickr_photo.sql
+++ OracleSpatialWeb/sql/PostgreSQL/DDL/flickr_photo.sql
@@ -10,12 +10,12 @@ CREATE TABLE flickr_photo
   latitude numeric(15,0) NOT NULL,
   taken_date date,
   upload_date date,
-  viewed numeric,
+  viewed integer,
   title character varying(255),
   smallurl character varying(100),
   place_id character varying(20),
   woe_id character varying(20),
-  accuracy numeric,
+  accuracy integer,
   CONSTRAINT flickr_photo_pk PRIMARY KEY (photo_id),
   CONSTRAINT flickr_photo_fk FOREIGN KEY (user_id)
       REFERENCES flickr_people (user_id) MATCH SIMPLE
