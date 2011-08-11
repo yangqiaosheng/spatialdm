5fa3d81e67f4e931958c63ceb9b0938b1eed9554
haolin.zhi <haolin.zhi@62b768d2-f648-7287-3a29-ed549c025e75>
2011-08-11 13:57:10 +0000

    JoinFlickrEuropeAreaCount.java add updateRegionId()
    support Radius: R320000-R375

---
+++ OracleSpatialWeb/sql/PostgreSQL/DML/flickr_europe-add_column_region_id.sql
@@ -0,0 +1,22 @@
+alter table flickr_europe 
+add column region_320000_id integer;
+alter table flickr_europe 
+add column region_160000_id integer;
+alter table flickr_europe 
+add column region_80000_id integer;
+alter table flickr_europe 
+add column region_40000_id integer;
+alter table flickr_europe 
+add column region_20000_id integer;
+alter table flickr_europe 
+add column region_10000_id integer;
+alter table flickr_europe 
+add column region_5000_id integer;
+alter table flickr_europe 
+add column region_2500_id integer;
+alter table flickr_europe 
+add column region_1250_id integer;
+alter table flickr_europe 
+add column region_750_id integer;
+alter table flickr_europe 
+add column region_375_id integer;
---
+++ OracleSpatialWeb/sql/PostgreSQL/DML/flickr_europe_count-init.sql
@@ -0,0 +1,3 @@
+delete from flickr_europe_count;
+insert into flickr_europe_count (id, radius) 
+select id, CAST(radius as integer) from flickr_europe_area;
