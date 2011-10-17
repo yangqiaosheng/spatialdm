48a499313b830aabe440773e82d6864c89160d16
haolin.zhi <haolin.zhi@62b768d2-f648-7287-3a29-ed549c025e75>
2011-10-17 14:03:20 +0000

    finish

--- OracleSpatialWeb/WebRoot/script/DragnDrop.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#idlegendContainer").draggable({ opacity: 1 });
-    $("#move").draggable({ handle: '#idmove' });
-    $("#maxContainer").draggable({ opacity: 1 });
-    $("#photoWindow").draggable({ opacity: 1 });
-    $("#pictureMouseOver").draggable({ opacity: 1 });     
-});
