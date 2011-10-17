151d9e423a86afd93624a97af139ee66f6646791
haolin.zhi <haolin.zhi@62b768d2-f648-7287-3a29-ed549c025e75>
2011-10-17 13:26:03 +0000

    Cancelable Job for Tag Servlet

---
+++ branches/europe/src/de/fraunhofer/iais/spatial/service/FlickrAreaCancelableJob.java
@@ -0,0 +1,201 @@
+package de.fraunhofer.iais.spatial.service;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.collections.MapUtils;
+
+import com.google.common.collect.Maps;
+
+import de.fraunhofer.iais.spatial.dao.FlickrAreaDao;
+import de.fraunhofer.iais.spatial.dao.jdbc.FlickrAreaDaoOracleJdbc;
+import de.fraunhofer.iais.spatial.dto.FlickrAreaDto;
+import de.fraunhofer.iais.spatial.dto.SessionMutex;
+import de.fraunhofer.iais.spatial.entity.FlickrArea;
+import de.fraunhofer.iais.spatial.entity.FlickrAreaResult;
+import de.fraunhofer.iais.spatial.entity.Histograms;
+import de.fraunhofer.iais.spatial.entity.FlickrArea.Radius;
+import de.fraunhofer.iais.spatial.util.DateUtil;
+
+public class FlickrAreaCancelableJob {
+
+	private FlickrAreaDao flickrAreaDao;
+
+	public FlickrAreaDao getAreaDao() {
+		return flickrAreaDao;
+	}
+
+	public void setAreaDao(FlickrAreaDao areaDao) {
+		this.flickrAreaDao = areaDao;
+	}
+
+	public List<FlickrArea> getAllAreas(Date sessionTimestamp, SessionMutex sessionMutex, Radius radius) throws InterruptedException {
+
+		List<FlickrArea> areas = new ArrayList<FlickrArea>();
+		for (int areaid : flickrAreaDao.getAllAreaIds(radius)) {
+			checkInterruption(sessionTimestamp, sessionMutex);
+			areas.add(flickrAreaDao.getAreaById(areaid, radius));
+		}
+
+		return areas;
+	}
+
+	public List<FlickrArea> getAreasByRect(Date sessionTimestamp, SessionMutex sessionMutex, double x1, double y1, double x2, double y2, Radius radius) throws InterruptedException {
+
+		List<FlickrArea> areas = new ArrayList<FlickrArea>();
+		for (int areaid : flickrAreaDao.getAreaIdsByRect(x1, y1, x2, y2, radius)) {
+			checkInterruption(sessionTimestamp, sessionMutex);
+			areas.add(flickrAreaDao.getAreaById(areaid, radius));
+		}
+
+		return areas;
+	}
+
+	/**
+	 * calculate a Summary Histograms DataSet for all FlickrArea
+	 */
+	public Histograms calculateSumHistogram(Date sessionTimestamp, SessionMutex sessionMutex, List<FlickrArea> areas, FlickrAreaDto areaDto) throws InterruptedException {
+
+		checkInterruption(sessionTimestamp, sessionMutex);
+		Histograms sumHistrograms = new Histograms();
+
+		Map<String, Integer> sumQueryStrData = Maps.newHashMap();
+		for (String queryStr : areaDto.getQueryStrs()) {
+			sumQueryStrData.put(queryStr, 0);
+		}
+
+		Thread.sleep(30);
+		for (FlickrArea area : areas) {
+			checkInterruption(sessionTimestamp, sessionMutex);
+
+			for (Map.Entry<String, Integer> e : area.getHoursCount().entrySet()) {
+				if (areaDto.getQueryStrs().contains(e.getKey())) {
+					sumQueryStrData.put(e.getKey(), e.getValue() + sumQueryStrData.get(e.getKey()));
+				}
+			}
+		}
+
+		Calendar calendar = DateUtil.createReferenceCalendar();
+		calendar.setLenient(false);
+
+		Map<Integer, Integer> sumYearData = sumHistrograms.getYears();
+		Map<Integer, Integer> sumMonthData = sumHistrograms.getMonths();
+		Map<Integer, Integer> sumDayData = sumHistrograms.getDays();
+		Map<Integer, Integer> sumHourData = sumHistrograms.getHours();
+		Map<Integer, Integer> sumWeekdayData = sumHistrograms.getWeekdays();
+
+		//set values
+		for (Map.Entry<String, Integer> e : sumQueryStrData.entrySet()) {
+
+			int hour = Integer.parseInt(e.getKey().substring(11, 13));
+			sumHourData.put(hour, e.getValue() + sumHourData.get(hour));
+
+			int day = Integer.parseInt(e.getKey().substring(8, 10));
+			sumDayData.put(day, e.getValue() + sumDayData.get(day));
+
+			int month = Integer.parseInt(e.getKey().substring(5, 7));
+			sumMonthData.put(month, e.getValue() + sumMonthData.get(month));
+
+			int year = Integer.parseInt(e.getKey().substring(0, 4));
+			sumYearData.put(year, e.getValue() + sumYearData.get(year));
+
+			calendar.set(Calendar.YEAR, Integer.parseInt(e.getKey().substring(0, 4)));
+			calendar.set(Calendar.MONTH, Integer.parseInt(e.getKey().substring(5, 7)) - 1);
+			calendar.set(Calendar.DAY_OF_MONTH, Integer.parseInt(e.getKey().substring(8, 10)));
+			int weekday = DateUtil.getWeekdayInt(calendar.getTime());
+			sumWeekdayData.put(weekday, e.getValue() + sumWeekdayData.get(weekday));
+		}
+
+		return sumHistrograms;
+	}
+
+	public void countSelected(Date sessionTimestamp, SessionMutex sessionMutex, List<FlickrAreaResult> areaResults, List<FlickrArea> areas, FlickrAreaDto areaDto) throws InterruptedException{
+
+		for (FlickrArea area : areas) {
+			checkInterruption(sessionTimestamp, sessionMutex);
+
+			FlickrAreaResult areaResult = new FlickrAreaResult(area);
+			areaResults.add(areaResult);
+
+			int selectCount = 0;
+			Map<String, Integer> counts = null;
+
+			switch (areaDto.getQueryLevel()) {
+			case HOUR:
+				counts = area.getHoursCount();
+				break;
+			case DAY:
+				counts = area.getDaysCount();
+				break;
+			case MONTH:
+				counts = area.getMonthsCount();
+				break;
+			case YEAR:
+				counts = area.getYearsCount();
+				break;
+			}
+
+			for (Map.Entry<String, Integer> e : counts.entrySet()) {
+				if (areaDto.getQueryStrs().contains(e.getKey())) {
+					selectCount += e.getValue();
+				}
+			}
+
+//			for (String queryStr : areaDto.getQueryStrs()) {
+//				if (counts.containsKey(queryStr)) {
+//					num += counts.get(queryStr);
+//				}
+//			}
+
+			areaResult.setSelectedCount(selectCount);
+		}
+
+	}
+
+	public FlickrAreaResult countTag(Date sessionTimestamp, SessionMutex sessionMutex, FlickrArea area, FlickrAreaDto areaDto) throws InterruptedException {
+
+		checkInterruption(sessionTimestamp, sessionMutex);
+		FlickrAreaResult areaResult = new FlickrAreaResult(area);
+
+		Map<String, Map<String, Integer>> hoursTagsCount = null;
+		Map<String, Integer> tagsCount = areaResult.getTagsCount();
+
+		if (MapUtils.isEmpty(area.getHoursTagsCount())) {
+			flickrAreaDao.loadHoursTagsCount(area);
+		}
+		hoursTagsCount = area.getHoursTagsCount();
+
+		System.out.println(hoursTagsCount);
+
+		int i = 0;
+		for (String queryStr : areaDto.getQueryStrs()) {
+			if (i++ % (24 * 31) == 0) {
+				checkInterruption(sessionTimestamp, sessionMutex);
+			}
+			if (hoursTagsCount.containsKey(queryStr)) {
+				Map<String, Integer> tags = hoursTagsCount.get(queryStr);
+				for (Map.Entry<String, Integer> e : tags.entrySet()) {
+					tagsCount.put(e.getKey(), MapUtils.getIntValue(tagsCount, e.getKey()) + e.getValue());
+				}
+			}
+		}
+		return areaResult;
+	}
+
+	private void checkInterruption(Date sessionTimestamp, SessionMutex sessionMutex) throws InterruptedException {
+		try {
+			Thread.sleep(1);
+		} catch (InterruptedException e1) {
+		}
+
+		synchronized (this) {
+			if (!sessionMutex.getTimestamp().equals(sessionTimestamp)) {
+				throw new InterruptedException("Interrupted after");
+			}
+		}
+	}
+
+}
---
+++ branches/europe/src/de/fraunhofer/iais/spatial/web/servlet/TagServlet.java
@@ -0,0 +1,207 @@
+package de.fraunhofer.iais.spatial.web.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang3.SerializationUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.google.common.collect.Lists;
+
+import de.fraunhofer.iais.spatial.dto.FlickrAreaDto;
+import de.fraunhofer.iais.spatial.dto.SessionMutex;
+import de.fraunhofer.iais.spatial.entity.FlickrArea;
+import de.fraunhofer.iais.spatial.entity.FlickrAreaResult;
+import de.fraunhofer.iais.spatial.entity.FlickrArea.Radius;
+import de.fraunhofer.iais.spatial.service.FlickrAreaMgr;
+import de.fraunhofer.iais.spatial.util.FlickrAreaUtil;
+import de.fraunhofer.iais.spatial.util.StringUtil;
+import de.fraunhofer.iais.spatial.util.XmlUtil;
+
+public class TagServlet extends HttpServlet {
+	/**
+	* Logger for this class
+	*/
+	private static final Logger logger = LoggerFactory.getLogger(TagServlet.class);
+
+	private static final long serialVersionUID = 289355222687198395L;
+
+	private static final int MAX_SIZE = 50;
+	private static final int DEFAULT_SIZE = 20;
+	private static FlickrAreaMgr areaMgr = null;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+	/**
+		 * The doGet method of the servlet. <br>
+		 *
+		 * This method is called when a form has its tag value method equals to get.
+		 *
+		 * @param request the request send by the client to the server
+		 * @param response the response send by the server to the client
+		 * @throws ServletException if an error occurred
+		 * @throws IOException if an error occurred
+		 */
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Date timestamp = new Date();
+		timestamp.setTime(NumberUtils.toLong(request.getParameter("timestamp")));
+
+		HttpSession session = request.getSession();
+		SessionMutex sessionMutex = null;
+		synchronized (this) {
+			if (session.getAttribute(HistrogramsDataServlet.HISTOGRAM_SESSION_ID) == null) {
+				session.setAttribute(HistrogramsDataServlet.HISTOGRAM_SESSION_ID, new SessionMutex(timestamp));
+			}
+			sessionMutex = (SessionMutex) session.getAttribute(HistrogramsDataServlet.HISTOGRAM_SESSION_ID);
+			if (sessionMutex.getTimestamp().before(timestamp)) {
+				sessionMutex.setTimestamp(timestamp);
+			}
+		}
+
+		response.setContentType("text/xml");
+		// Prevents caching
+		response.setHeader("Cache-Control", "no-store"); // HTTP1.1
+		response.setHeader("Pragma", "no-cache"); // HTTP1.0
+		response.setDateHeader("Expires", 0); // proxy server
+
+		String areaid = request.getParameter("areaid");
+		int size = NumberUtils.toInt(request.getParameter("size"), DEFAULT_SIZE);
+		PrintWriter out = response.getWriter();
+
+		Document document = new Document();
+		Element rootElement = new Element("response");
+		document.setRootElement(rootElement);
+		Element messageElement = new Element("message");
+		rootElement.addContent(messageElement);
+
+		logger.info("doGet(HttpServletRequest, HttpServletResponse) - areaid:" + areaid + "|size:" + size); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+
+		if (!StringUtils.isNumeric(areaid)) {
+			messageElement.setText("ERROR: wrong input parameter!");
+		} else if (request.getSession().getAttribute("areaDto") == null) {
+			messageElement.setText("ERROR: please perform a query first!");
+		} else {
+			try {
+				FlickrAreaDto areaDto = SerializationUtils.clone((FlickrAreaDto) request.getSession().getAttribute("areaDto"));
+				int zoom = NumberUtils.toInt(request.getParameter("zoom"), areaDto.getZoom());
+				Radius radius = FlickrAreaUtil.judgeRadius(zoom);
+
+				logger.debug("doGet(HttpServletRequest, HttpServletResponse) - areaid:" + areaid + "|radius:" + radius ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+
+				FlickrArea area = areaMgr.getAreaDao().getAreaById(Integer.parseInt(areaid), Radius.valueOf("R" + radius));
+
+				if(session.getAttribute(HistrogramsDataServlet.HISTOGRAM_SESSION_LOCK) != null){
+					int waitSec = 5;
+					for (int i = 1; i <= waitSec; i++) {
+						Thread.sleep(1000);
+						if (session.getAttribute(HistrogramsDataServlet.HISTOGRAM_SESSION_LOCK) == null && sessionMutex.getTimestamp().equals(timestamp)) {
+							break;
+						} else {
+							if (i == waitSec) {
+								throw new TimeoutException("Blocked until:" + waitSec + "s");
+							}
+							if (!sessionMutex.getTimestamp().equals(timestamp)) {
+								throw new InterruptedException("Interrupted before");
+							}
+						}
+					}
+				}
+
+				session.setAttribute(HistrogramsDataServlet.HISTOGRAM_SESSION_LOCK, new SessionMutex(timestamp));
+
+				if(area != null){
+					tagsResponseXml(timestamp, sessionMutex, document, area, areaDto, size);
+					messageElement.setText("SUCCESS");
+				}else{
+					messageElement.setText("ERROR: the request polygon doesn't exist in the current zoom level!");
+				}
+				session.removeAttribute(HistrogramsDataServlet.HISTOGRAM_SESSION_ID);
+			} catch (TimeoutException e) {
+				messageElement.setText("INFO: Rejected until Timeout!");
+				rootElement.addContent(new Element("exceptions").setText(StringUtil.printStackTrace2String(e)));
+				rootElement.addContent(new Element("description").setText(e.getMessage()));
+			} catch (InterruptedException e) {
+				messageElement.setText("INFO: interupted by another query!");
+//				rootElement.addContent(new Element("exceptions").setText(StringUtil.printStackTrace2String(e)));
+//				rootElement.addContent(new Element("description").setText(e.getMessage()));
+			} catch (Exception e) {
+				logger.error("doGet(HttpServletRequest, HttpServletResponse)", e); //$NON-NLS-1$
+				messageElement.setText("ERROR: wrong input parameter!");
+//				rootElement.addContent(new Element("exceptions").setText(StringUtil.printStackTrace2String(e)));
+				rootElement.addContent(new Element("description").setText(e.getMessage()));
+			} finally {
+				session.removeAttribute(HistrogramsDataServlet.HISTOGRAM_SESSION_LOCK);
+			}
+		}
+
+		out.print(XmlUtil.xml2String(document, false));
+
+		out.flush();
+		out.close();
+		System.gc();
+	}
+
+	private String tagsResponseXml(Date timestamp, SessionMutex sessionMutex, Document document, FlickrArea area, FlickrAreaDto areaDto, int size) throws InterruptedException {
+
+		FlickrAreaResult areaResult = areaMgr.getAreaCancelableJob().countTag(timestamp, sessionMutex, area, areaDto);
+
+		List<Map.Entry<String, Integer>> entries = Lists.newLinkedList(areaResult.getTagsCount().entrySet());
+
+		//sort list based on comparator
+		Collections.sort(entries, new Comparator<Map.Entry<String, Integer>>() {
+			@Override
+			public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+				return e2.getValue() - e1.getValue();
+			}
+		});
+
+		Element tagsElement = new Element("tags");
+		document.getRootElement().addContent(tagsElement);
+
+		int num = 0;
+		for(Map.Entry<String, Integer> entry : entries){
+			if(num++ > size){
+				break;
+			}
+			Element tagElement = new Element("tag");
+			tagsElement.addContent(tagElement);
+			tagElement.setAttribute("name", entry.getKey());
+			tagElement.setAttribute("num", String.valueOf(entry.getValue()));
+		}
+
+		return XmlUtil.xml2String(document, true);
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException
+	 *             - if an error occurs
+	 */
+	@Override
+	public void init() throws ServletException {
+		areaMgr = WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext()).getBean("flickrAreaMgr", FlickrAreaMgr.class);
+	}
+}
